9c42b0cf217dce10004e025484ff8132981a04df
lizhongyidexiao <liyangyang_haobost@163.com>
2021-01-05 21:30:03 +0800

    C_test_1-4

---
+++ switch case.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+	int a=0;
+	
+	scanf("%d",&a);
+	
+	
+	
+	switch(a)
+     {
+	 case 1:
+	 printf("一帆风顺");
+	break;	
+	case 2:
+	printf("二龙戏珠");
+	break;
+	default:
+	printf("啥玩意");
+	
+	
+	
+}
+	
+	return 0;
+}
---
+++ 打印100以内奇偶数.cpp
@@ -0,0 +1,15 @@
+
+#include<stdio.h>
+int main(void)
+{
+	
+	
+	
+	
+	int i;
+	for(i=0;i<101;i++)
+	  
+	  if (i%2==1)
+	  printf("%d\n",i);
+	  return 0;
+}
